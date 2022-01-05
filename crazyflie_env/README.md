173d267e26e07cac4bf61190d1ddad229bc9c467
Cheng <c.liu-10@tudelft.nl>
2022-01-05 22:37:50 +0100

    add initial files

---
+++ crazyflie_env/README.md
@@ -0,0 +1,31 @@
+# gym-nav2D-hybrid
+
+A UAV 2D navigation environment with hybrid action space built on OpenAI gym.
+
+## Dependencies
+
++ python 3.5+
++ gym 0.10.5
++ pygame 1.9.4
++ numpy 1.19.5
+
+## Installation
+
+```shell
+git clone https://github.com/chengliu-LR/gym-nav2D-hybrid.git
+cd gym-nav2D-hybrid
+python3 -m pip install -e '.[gym-nav2D-hybrid]'
+```
+
+## Demo
+
+```python
+import gym, gym_nav2d
+env = gym.make('nav2d-v0') # env id
+```
+
+## TODO
+- [x] Implement `step()` method
+- [x] Implement `reset()` method
+- [x] More reasonable observation_space and action_space
+- [ ] P-DQN algorithm implementation
---
+++ crazyflie_env/envs/crazyflie_env.py
@@ -0,0 +1,28 @@
+"""
+Classic cart-pole system implemented by Rich Sutton et al.
+Copied from http://incompleteideas.net/sutton/book/code/pole.c
+permalink: https://perma.cc/C9ZM-652R
+"""
+
+import math
+from typing import Optional
+
+import gym
+from gym import spaces, logger
+from gym.utils import seeding
+import numpy as np
+
+
+class CrazyflieEnv(gym.Env):
+
+    def __init__(self):
+        self.state = 0
+        self.action_space = spaces.Discrete(2)
+
+    def step(self, action):
+        reward = self.state + action
+        return np.array(self.state, dtype=np.float32), reward, None, {}
+
+    def reset(self):
+        self.state = 0
+        return np.array(self.state, dtype=np.float32)
