dd27dac29d3ea0a07a17fbd89a7bda280a2c5b5b
Cheng <c.liu-10@tudelft.nl>
2022-01-11 21:37:11 +0100

    utils for crazyflie env like action and state spaces, function of robots and point to segment distance cal-func

---
+++ crazyflie_env/envs/utils/robot.py
@@ -0,0 +1,90 @@
+import math
+import logging
+from gym.utils import seeding
+import gym
+import numpy as np
+from crazyflie_env.envs.utils.action import ActionXY
+from crazyflie_env.envs.utils.state import ObservableState, FullState
+
+
+class Robot():
+    def __init__(self, config=None):
+        self.px = None
+        self.py = None
+        self.gx = None
+        self.gy = None
+        self.vx = None
+        self.vy = None
+        self.radius = 0.1
+        self.v_pref = 1.0 # max velocity that can choose
+        self.time_step = None
+
+
+    def set_state(self, px, py, gx, gy, vx, vy, radius=None):
+        """
+        Set initial state for the robot.
+        Param: (position_x, position_y, goal_pos_x, goal_pos_y, vel_x, vel_y, [optional] radius).
+        """
+        self.px = px
+        self.py = py
+        self.gx = gx
+        self.gy = gy
+        self.vx = vx
+        self.vy = vy
+        if radius is not None:
+            self.radius = radius
+
+
+    def get_full_state(self):
+        return FullState(self.px, self.py, self.vx, self.vy, self.radius, self.gx, self.gy)
+    
+
+    def get_next_full_state(self, action):
+        self.validate_action(action)
+        next_px, next_py = self.compute_next_position(action, self.time_step)
+        next_vx = action.vx
+        next_vy = action.vy
+        return FullState(next_px, next_px, next_vx, next_vy, self.radius, self.gx, self.gy)
+
+
+    def get_position(self):
+        return self.px, self.py
+
+
+    def set_position(self, position):
+        self.px = position[0]
+        self.py = position[1]
+
+
+    def get_goal_position(self):
+        return self.gx, self.gy
+
+
+    def get_velocity(self):
+        return self.vx, self.vy
+
+
+    def set_velocity(self, velocity):
+        self.vx = velocity[0]
+        self.vy = velocity[1]
+
+    def compute_next_position(self, action, dt):
+        px = self.px + action.vx * dt
+        py = self.py + action.vy * dt
+        return px, py
+
+
+    def validate_action(self, action):
+        assert isinstance(action, ActionXY)
+
+
+    def step(self, action):
+        self.validate_action(action)
+        self.px, self.py = self.compute_next_position(action, self.time_step)
+        self.vx = action.vx
+        self.vy = action.vy
+        return self.get_full_state()
+
+
+    def reached_destination(self):
+        return np.linalg.norm(np.array(self.get_position()) - np.array(self.get_goal_position())) < self.radius
---
+++ crazyflie_env/envs/utils/state.py
@@ -0,0 +1,48 @@
+# JointState = ObservableState + FullState
+class ObservableState(object):
+    def __init__(self, px, py, vx, vy, radius):
+        self.px = px
+        self.py = py
+        self.vx = vx
+        self.vy = vy
+        self.radius = radius
+
+        self.position = (self.px, self.py)
+        self.velocity = (self.vx, self.vy)
+
+    def __add__(self, other):
+        return other + (self.px, self.py, self.vx, self.vy, self.radius)
+
+    def __str__(self):
+        return ' '.join([str(x) for x in [self.px, self.py, self.vx, self.vy, self.radius]])
+
+
+class FullState():
+    def __init__(self, px, py, vx, vy, radius, gx, gy):
+        self.px = px
+        self.py = py
+        self.vx = vx
+        self.vy = vy
+        self.radius = radius
+        self.gx = gx
+        self.gy = gy
+
+        self.position = (self.px, self.py)
+        self.goal_position = (self.gx, self.gy)
+        self.velocity = (self.vx, self.vy)
+
+    def __add__(self, other):
+        return other + (self.px, self.py, self.vx, self.vy, self.radius, self.gx, self.gy)
+
+    def __str__(self):
+        return ' '.join([str(x) for x in [self.px, self.py, self.vx, self.vy, self.radius, self.gx, self.gy]])
+
+
+class JointState():
+    def __init__(self, self_state, obstacle_states):
+        assert isinstance(self_state, FullState)
+        for obstacle_state in obstacle_states:
+            assert isinstance(obstacle_state, ObservableState)
+        
+        self.self.state = self_state
+        self.obstacle_states = obstacle_states
